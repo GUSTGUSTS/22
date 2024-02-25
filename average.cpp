ff4d584db68291f8c51bd00a2f94a856e94f7107
GUSTGUSTS <141816627+GUSTGUSTS@users.noreply.github.com>
2024-02-25 21:41:50 +0700

    Update average.cpp finish
    done

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
 
+int main(int argc, char** argv){
+ int a = argc-1;
+ double x = 0;
+ for(int i=1; i<=a;i++){
+ x += atof(argv[i]);
+ }
+ x /= a;
+ if(a == 0){cout << "Please input numbers to find average.";}else{
+ cout << "---------------------------------\n";
+ cout << "Average of " << a << " numbers = " << x;
+ cout << "\n---------------------------------";}
+}
